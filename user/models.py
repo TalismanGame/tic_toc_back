d006ee5eb1693a7a0af26909c3a2aaa08ece09f7
mohamad <mohamad@Audrey.local>
2021-11-20 23:17:11 +0330

    changes on user model

--- user/models.py
+++ user/models.py
@@ -1,41 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 
+
 # Create your models here.
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+
+    def create_user(self, username, email, password=None):
+
+        if not username:
+            raise TypeError('Users must have a username.')
+
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            email=self.normalize_email(email),
-            name=self.normalize_email(email),
+            username = username,
+            email = self.normalize_email(email),
         )
 
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-        )
-        user.is_admin = True
-        user.is_active=True
-        user.is_staff = True
+    def create_superuser(self, username, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        email = email.lower()
+        user = self.create_user(username, email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
+
+        return user
 
 class MyUser(AbstractUser, PermissionsMixin):
     username = models.CharField(max_length=255, blank=False, null=False, unique=True)
+    email = models.EmailField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_superuser = models.BooleanField(default=False)
-    email = models.EmailField(db_index=True)
 
     USERNAME_FIELD = "username"
-
 
     objects = MyUserManager()
 
@@ -43,8 +53,5 @@ class MyUser(AbstractUser, PermissionsMixin):
         db_table = "tbl_users"
 
     def __str__(self):
-        return self.username
+        return str(self.username)
         
-    def has_perm(self, perm, obj=None): return self.is_superuser
-
-    def has_module_perms(self, app_label): return self.is_superuser
