27eadba8d5bdacaf8adb2c290f2b3264ebb0a5c8
audrey <mbozorgi604@gmail.com>
2022-01-13 18:56:05 +0330

    add game model

--- game/models.py
+++ game/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -8,3 +9,30 @@ class Player(models.Model):
 
     def __str__(self):
         return self.alias
+
+class Game(models.Model):
+    gameBoard = ArrayField(
+         models.CharField(max_length=1),
+         default=[
+            None, None, None,
+            None, None, None,
+            None, None, None
+        ],
+        size=9
+    )
+    status = models.CharField(max_length=32, null=True)
+    playerX = models.ForeignKey(Player, related_name="x", on_delete=models.CASCADE, null=True)
+    playerO = models.ForeignKey(Player, related_name="O", on_delete=models.CASCADE, null=True)
+    nextPlayer = models.CharField(max_length=1, default='X')
+    xState =  models.CharField(max_length=16, default='not_ready')
+    oState =  models.CharField(max_length=16, default='not_ready')
+    winner = models.CharField(max_length=1, null=True)
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (
+            self.tiles,
+            self.state,
+            self.playerNext,
+            self.playerX,
+            self.playerO
+        )
