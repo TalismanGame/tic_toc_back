c7027a4b52a9bfb4e2d9af28df67dd7b122dcebe
audrey <mbozorgi604@gmail.com>
2022-02-04 19:22:48 +0330

    some other updates on game model and choices replaces to simple text

--- game/models.py
+++ game/models.py
@@ -27,11 +27,27 @@ class Game(models.Model):
     DELETED = 2
     FINISHED = 3
 
+    PLAYER_X = 0
+    PLAYER_O = 1
+
+    DRAW = 3
+
     GAME_STATE = [
         (WAITING, 'WAITING'),
         (READY, 'READY'),
         (DELETED,'DELETED'),
         (FINISHED,'FINISHED'),
+    ]
+
+    PLAYERS = [
+        (PLAYER_X, 'PLAYER_X'),
+        (PLAYER_O, 'PLAYER_O'),
+    ]
+
+    WIN_OPTIONS  = [
+        (PLAYER_X, 'X'),
+        (PLAYER_O, 'O'),
+        (DRAW, 'DRAW'),
     ]
 
     gameBoard = ArrayField(
@@ -43,13 +59,13 @@ class Game(models.Model):
          ]),
         size=9
     )
-    status = models.CharField(choices=GAME_STATE, max_length=32, default='WAITING')
+    status = models.CharField(choices=GAME_STATE, max_length=32)
     playerX = models.ForeignKey(Player, related_name="games_created", on_delete=models.CASCADE, null=True)
     playerO = models.ForeignKey(Player, related_name="games_invited", on_delete=models.CASCADE, null=True)
-    nextPlayer = models.CharField(max_length=1, default='X')
+    nextPlayer = models.CharField(choices=PLAYERS, max_length=1, default='X')
     xState =  models.CharField(choices=Player.PLAYER_STATE, max_length=16, default='NOT_READY')
     oState =  models.CharField(choices=Player.PLAYER_STATE, max_length=16, default='NOT_READY')
-    winner = models.CharField(max_length=1, null=True)
+    winner = models.CharField(choices=WIN_OPTIONS, max_length=1, null=True)
     inviteCode = models.CharField(unique=True, max_length=10, null=True)
 
     def __str__(self):
