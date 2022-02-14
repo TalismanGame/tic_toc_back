f15d0e44e2f9141a5f596f280d3af20dc95245da
audrey <mbozorgi604@gmail.com>
2022-02-14 11:40:21 +0330

    add important notes

--- game/admin.py
+++ game/admin.py
@@ -3,6 +3,10 @@ from .models import Player, Game
 
 
 # can also use decorator to register instead of admin.site.register. both work the same way
+# You can’t use this decorator if you have to reference your model admin class in its __init__() method, e.g. super(PersonAdmin, self).__init__(*args, **kwargs). You can use super().__init__(*args, **kwargs).
+# https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
+
+
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ["id", "alias", ]
