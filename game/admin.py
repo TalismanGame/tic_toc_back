4ad63192c6f6dd2a9ca6e92abf9868bff3bfd8d4
audrey <mbozorgi604@gmail.com>
2022-02-14 11:39:19 +0330

    add decorator instead of admin register

--- game/admin.py
+++ game/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import Player, Game
 
+
+# can also use decorator to register instead of admin.site.register. both work the same way
+@admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ["id", "alias", ]
-admin.site.register(Player, PlayerAdmin)
+# admin.site.register(Player, PlayerAdmin)
 
-
+@admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     #question why this fields get empty after i add choices to them
     list_display = ["playerX", "xState", "playerO", "oState", "status", "inviteCode"]
     search_fields = ['playerX__alias', "playerO__alias"]
 
-admin.site.register(Game, GameAdmin)
+# admin.site.register(Game, GameAdmin)
