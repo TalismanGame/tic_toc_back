d0b99ed32759a292b6f671ee4a41f263b024f0e2
audrey <mbozorgi604@gmail.com>
2022-03-04 23:24:43 +0330

    change asgi.py file

--- tic_toc_back/asgi.py
+++ tic_toc_back/asgi.py
@@ -1,11 +1,10 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import game.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_toc_back.settings')
@@ -17,22 +16,4 @@ application = ProtocolTypeRouter({
             game.routing.websocket_urlpatterns
         )
     ),
-    # Just HTTP for now. (We can add other protocols later.)
 })
-
-
-
-
-
-# import os
-
-# import django
-# from channels.http import AsgiHandler
-# from channels.routing import ProtocolTypeRouter,get_default_application
-
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_toc_back.settings')
-# django.setup()
-
-
-# application=get_default_application()
